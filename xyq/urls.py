28054dd9ed4c28dab003fad08b26671d7144f37d
EnderBoy666 <a157031601@163.com>
2025-03-15 14:25:48 +0800

    1.6.1

--- xyq/urls.py
+++ xyq/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
     path('',include('xyq_files.urls')),
+    path('password-change-done/', PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
 ]
